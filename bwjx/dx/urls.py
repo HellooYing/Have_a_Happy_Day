1bfb31fc3b2caf64e1909189a52632bec2eeaffe
hellooying <15642330153@163.com>
2018-07-28 20:44:52 +0800

    把报文解析拿出来，等安卓demo写好后放一起开个新仓库

--- bwjx/dx/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic.base import RedirectView
-app_name = 'dx'
-
-urlpatterns = [
-    url(r'^$',views.test,name='test'),
-    url(r'^test1/$', views.test1, name='test1'),
-]
